a8871a8064b23ae24cdde42e9d4487425e859b5b
Marcello Bardus <marcellobardus8@gmail.com>
2020-10-08 11:54:38 +0200

    hello world

--- test/helloworld.js
+++ test/helloworld.js
@@ -1,10 +1,12 @@
-const { it } = require("ethers/wordlists");
-
 const HelloWorld = artifacts.require("HelloWorld");
 
 contract("Hello World test", async (accounts) => {
   it("Should set a greeting", async () => {
     const instance = await HelloWorld.deployed();
     await instance.setGreeting("Hello world");
+
+    const greeting = await instance.getGreeting();
+
+    assert.equal(greeting, "Hello world");
   });
 });
